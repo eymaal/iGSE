8a2c94b8ec42302c4ce010bebd88b53ed1d1c85a
Amal Jos <ajc99@student.le.ac.uk>
2022-12-26 19:36:51 +0000

    Initial Commit

---
+++ src/main/java/com/mobileweb/igse/IGseApplication.java
@@ -0,0 +1,13 @@
+package com.mobileweb.igse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IGseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IGseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mobileweb/igse/IGseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobileweb.igse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IGseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
